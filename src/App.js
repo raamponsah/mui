1f4c2ae0b1bf65788c41389b5b98a4009711ab03
Ralph <ramponsah026@st.ug.edu.gh>
2023-03-13 17:10:25 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Button } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Person } from "@mui/icons-material";
+import { PersonPinCircle } from "@mui/icons-material";
 
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
-      <Button variant="contained" color="error" startIcon={<DeleteIcon/>} endIcon={<Person/>}>Press ME</Button>
+      <Button variant="contained" size="large" color="info" startIcon={<DeleteIcon/>} endIcon={<PersonPinCircle/>}>Press ME</Button>
     </div>
   );
 }
