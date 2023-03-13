1468331ed66f6532819601dfc5383fb224b37400
Ralph <ramponsah026@st.ug.edu.gh>
2023-03-13 17:08:33 +0000

    Learning MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Person } from "@mui/icons-material";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <Button variant="contained" color="error" startIcon={<DeleteIcon/>} endIcon={<Person/>}>Press ME</Button>
     </div>
   );
 }
